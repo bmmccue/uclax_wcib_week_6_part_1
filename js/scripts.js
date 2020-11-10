541193f6bb493cee1313298ca4c874eebaa61bf1
Apple <apple@apples-air.home>
2020-11-10 10:18:34 -0800

    recent changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
 console.log('Hello from js/scripts.js!');
+
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+
+for (let i = 1; i < 7; i++){
+
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', '/images/pic' + i + '.jpg');
+  thumbBar.appendChild(newImage);
+  newImage.onclick = function(){
+    newImage.setAttribute('src', '/images/pic' + i + '.jpg')
+
+  }
+}
+
+/* Wiring up the Darken/Lighten button */
